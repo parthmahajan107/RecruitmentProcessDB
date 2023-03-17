f7f78aeb842717539154a83e21995f804f7e2947
parth <parth@PARTH>
2023-03-17 20:11:00 +0530

    Add project files.

---
+++ RecruitmentProcessDB/Candidate.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Candidate]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(100) NOT NULL, 
+    [MiddleName] NVARCHAR(100) NOT NULL, 
+    [LastName] NVARCHAR(100) NOT NULL, 
+    [dob] DATETIME NOT NULL, 
+    [Mobile] NVARCHAR(10) NULL, 
+    [EmailId] NVARCHAR(200) NULL
+)
---
+++ RecruitmentProcessDB/Recruiter.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Recruiter]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(100) NOT NULL, 
+    [MiddleName] NVARCHAR(100) NOT NULL, 
+    [LastName] NVARCHAR(100) NOT NULL, 
+    [Mobile] NVARCHAR(10) NULL, 
+    [EmailId] NVARCHAR(200) NULL
+)
